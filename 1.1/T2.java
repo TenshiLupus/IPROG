2b3af382f68cac141d66ef6aa00ce94c1e7fa309
Angel Cardenas Martinez <TenshiLupus@hotmail.com>
2022-09-19 13:19:45 +0200

    1.1 done

--- 1.1/T2.java
+++ 1.1/T2.java
@@ -1,24 +1,38 @@
 
-
-public class T2 implements Runnable{
+public class T2 implements Runnable {
     Thread currentThread = new Thread(this);
 
     String name;
     int sleepTime;
+    boolean active = true;
 
-    public T2(String name, int sleepTime){
+    public T2(String name, int sleepTime) {
         this.name = name;
         this.sleepTime = sleepTime;
         currentThread.start();
-    } 
+    }
 
     @Override
     public void run() {
-        try{
-        System.out.printf("im T2 sleeping for: %s seconds\n", this.sleepTime);
-        }catch(Exception e){
-            System.out.println(e.getMessage());
+        while (currentThread.isAlive()) {
+            while (active) {
+                try {
+                    System.out.println("Tråd 2");
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+            // try{
+            //     System.out.println("im T2, sleeping for: 5 seconds");
+            //     Thread.sleep(sleepTime);
+            // }catch(InterruptedException ie){
+            //     System.out.println(ie.getMessage());
+            // }
         }
     }
 
+    public void flipActiveMode() {
+        this.active = !active;
+    }
 }
--- 1.1/Threadsinitiator.java
+++ 1.1/Threadsinitiator.java
@@ -2,8 +2,41 @@ import java.util.*;
 
 public class Threadsinitiator {
 
-    public static void main(String[] args){
+    // Main will act as the main thread of execution
+    public static void main(String[] args) {
+        // create first thread
         T1 tone = new T1("tone", 5000);
-        T2 ttwo = new T2("ttwo");
+        tone.start();
+
+        // sleep main thread for 5 seconds
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ie) {
+            System.out.println(ie.getMessage());
+        }
+
+        // create second thread
+        T2 ttwo = new T2("ttwo", 5000);
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ie) {
+            System.out.println(ie.getMessage());
+        }
+
+        //switch active mode in thread one
+        tone.flipActiveMode();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ie) {
+            System.out.println(ie.getMessage());
+        }
+
+        //switch active mode in thread two
+        ttwo.flipActiveMode();
+
+        //notify end of program
+        System.out.println("program ending");
     }
 }
