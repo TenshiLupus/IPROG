296391f13f89ccac088425782419ad09239e690b
Tenshilupus <Angel__CM@hotmail.com>
2022-10-19 19:58:01 +0200

    websocket json response type defined

--- gesällprov/wsp/utility/handlers/handlers.go
+++ gesällprov/wsp/utility/handlers/handlers.go
@@ -3,7 +3,9 @@ package handlers
 import (
 	"log"
 	"net/http"
+
 	"github.com/CloudyKit/jet/v6"
+	"github.com/gorilla/websocket"
 )
 
 var views = jet.NewSet(
@@ -11,11 +13,38 @@ var views = jet.NewSet(
 	jet.InDevelopmentMode(),
 )
 
+var upgradeConnection = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
 func Home(w http.ResponseWriter, r *http.Request) {
 	err := renderPage(w, "home.jet", nil)
 	if err != nil {
-		log.Println(err) 
+		log.Println(err)
 	}
+}
+
+// WsJsonResponse defines the response sent back from websocket
+type WsJsonResponse struct {
+	Action      string `json:"action"`
+	Message     string `json:"message"`
+	MessageType string `json:"messge_type"`
+}
+
+// WsEndPoint upgrades connection to websocket
+func WsEndpoint(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgradeConnection.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Client connected to endpoint")
+
+	//instantiating an empty instance of WsJsonResponse
+	var response WsJsonResponse
+	response.Message = `<em><small>Connected to Server</small></em>`
 }
 
 func renderPage(w http.ResponseWriter, tmpl string, data jet.VarMap) error {
