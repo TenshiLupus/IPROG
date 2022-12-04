669babca74f44355c62c0bcd2f387b80792c3d64
Tenshilupus <Angel__CM@hotmail.com>
2022-12-04 13:13:57 +0100

    Comments added

--- 4.1.1/EncryptionDecryption/src/DecryptHandler.java
+++ 4.1.1/EncryptionDecryption/src/DecryptHandler.java
@@ -14,52 +14,36 @@ public class DecryptHandler {
     
     public static void main(String[] args) throws NoSuchAlgorithmException, IOException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, ClassNotFoundException {
         
+        //Settings that are used for the Cipher
         String algorithm = "AES";
         String cipherEngine = "AES/ECB/PKCS5Padding";
-       
-        // FileInputStream encryptedFileData = new FileInputStream(args[0]);
-        // byte[] encryptedData = encryptedFileData.readAllBytes();
-        // encryptedFileData.close();
 
-        // System.out.println(encryptedData.length);
-
-
-
+        //Retrieve the unencrypted key from the file
         ObjectInputStream ois = new ObjectInputStream(new FileInputStream(args[1]));
         String b64encodedKey = (String) ois.readObject(); 
         ois.close();
 
+        //Formatting key data from the file into a compatible key format.
         byte[] b64decodedKey = Base64.getDecoder().decode(b64encodedKey);
         Key originalKey = new SecretKeySpec(b64decodedKey, 0, b64decodedKey.length, algorithm);
 
+        //Initiate the cipher that will be used for decryption
         Cipher cipher = Cipher.getInstance(cipherEngine);
         cipher.init(Cipher.DECRYPT_MODE, originalKey);
 
-
+        //Due to stream corruption with "cipher.doFinal()", a CipherInputStream is being used as an alternative
         CipherInputStream cis = new CipherInputStream(new FileInputStream(args[0]), cipher);
         BufferedReader br = new BufferedReader(new InputStreamReader(cis));
-        System.out.println("Message received");
-        // byte[] decryptedDataContent = cis.readAllBytes();
-
-
-        // String formattedMessage = new String(decryptedDataContent);
-        // System.out.println("DECRYPTION: " + formattedMessage);
-        // byte[] decryptedOutput = cipher.doFinal(encryptedData);
         
-
-        // System.out.println(decryptedOutput);
-
-        System.out.println("Input: " + args[0]);
-
+        //Prepare writing to a file with the same format for this use case
         OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(args[2]+".txt"));
         PrintWriter pw = new PrintWriter(osw);
 
-        pw.println("before Writing Content");
+        //Writes retrieved content into the output file.
         String msg;
         while ((msg = br.readLine()) != null){
             pw.println(msg);
         }
-        pw.println("After Writing Content");
 
         osw.close();
         pw.close();
